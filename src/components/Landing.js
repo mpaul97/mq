e94495dd91304689d9861865a3529f7771b6e9d9
Michael Paul <michaelpaul@Mac.grandenetworks.net>
2025-02-05 16:48:07 -0600

    first commit

---
+++ src/components/Landing.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import content from "../assets/content.json";
+import media from "../assets/media.jpeg";
+
+const LINES = content['lines'];
+
+function Landing() {
+    const [index, setIndex] = useState(0);
+    const [height, setHeight] = useState(100);
+    const handleClick = () => {
+        if (index < LINES.length) {
+            setIndex(index + 1);
+            setHeight(height + 10);
+        }
+    };
+    return (
+        <div style={{
+                display: "flex",
+                flexDirection: "column",
+                color: "white",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                height: index !== LINES.length ? `${height}vh` : "100%"
+            }} 
+            onClick={handleClick}
+        >
+            {LINES.slice(0, index).map((x, i) => {
+                return (
+                    <h3 
+                        style={{
+                            fontFamily: "Times New Roman",
+                            fontSize: i !== 10 ? 50 : 30,
+                            textAlign: "center"
+                        }}
+                    >
+                        {x}
+                    </h3>
+                )
+            })}
+            {
+                index === LINES.length ?
+                <img src={media} alt="image" /> : <></>
+
+            }
+        </div>
+    )
+};
+
+export default Landing;
